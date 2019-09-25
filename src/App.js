19d2a1b1336ba8ef6c9955fc75cc1f005a47daa5
Kyle Nelson <kyle.st.aubyn@gmail.com>
2019-09-25 08:03:02 +0800

    update app.js and app.css

--- src/App.js
+++ src/App.js
@@ -5,20 +5,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id = "space">
+           <h3>Apps make everyone become replaceable. Everyone becomes disposable. Is it possible to make a connection, let alone a commitment with someone when you know the next best thing is just a few swipes away? And is it possible to really let your guard down and let yourself truly fall for someone when you feel like you could be so easily replaced? - Sophie Milner</h3>
+           <div id = "titles">
+            <ul>
+                <li>User Layer </li>
+                <li>Interface Layer </li>
+                <li>Address Layer </li>
+                <li>City Layer </li>
+                <li>Earth Layer </li>
+            </ul>
+           </div>
+           <div id = "pics">
+            <img src="https://cdn1.imggmi.com/uploads/2019/9/18/91822c4a2016a30524cf4ddcbd389ed8-full.jpg" id = "user"/><img src="https://media.giphy.com/media/L2TxkfWhrFi1HWsjOE/source.gif"/><img src="https://cdn1.imggmi.com/uploads/2019/9/18/99fadc08d2e88faac7d65d7030ca7f5f-full.jpg"/><img src="https://media3.giphy.com/media/jMa7nDQYpD9za/giphy.gif?cid=790b7611c5844dc4ea6037c359b804219cb18f633ae823c1&rid=giphy.gif"/><img src="https://media0.giphy.com/media/l49JC5JvmDm17Exe8/giphy.gif?cid=790b761122b03f4e36b2a1a8545ccc8bd9431fc01fd00f51&rid=giphy.gif"/>
+           </div>
+        </div>
     </div>
   );
 }
